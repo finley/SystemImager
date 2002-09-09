ee036f033c4e9de9bc171625d8ca81c2cedc9109
brianfinley <brianfinley@8b493298-60f9-0310-b619-f4cd4bc255a3>
2002-09-09 21:16:01 +0000

    Initial commit.

---
+++ lib/SystemImager/Config.pm
@@ -0,0 +1,28 @@
+#
+# "SystemImager"
+#
+#  Copyright (C) 2002 Bald Guy Software 
+#                     Brian E. Finley <brian.finley@baldguysoftware.com>
+#
+#    $Id$
+#
+
+package SystemImager::Config;
+
+use strict;
+use AppConfig;
+
+
+my $config = AppConfig->new(
+    'autoinstall_script_dir' => { ARGCOUNT => 1 },
+    'autoinstall_boot_dir' => { ARGCOUNT => 1 },
+    'default_image_dir' => { ARGCOUNT => 1 },
+    'rsyncd_conf' => { ARGCOUNT => 1 },
+    'rsync_stub_dir' => { ARGCOUNT => 1 },
+    'tftp_dir' => { ARGCOUNT => 1 },
+);
+
+$config->file('/etc/systemimager/systemimager.conf');
+
+$::main::config = $config;
+
