acc1ac832863e7af649275a0dbff361d2fcf5273
brianfinley <brianfinley@8b493298-60f9-0310-b619-f4cd4bc255a3>
2003-03-07 06:55:23 +0000

    Add known vars so we can assume the var at least exists when we're testing for settings.

--- lib/SystemImager/Flamethrower.pm
+++ lib/SystemImager/Flamethrower.pm
@@ -35,6 +35,7 @@ sub read_config {
             CREATE => 1,
             GLOBAL => {
                 ARGCOUNT => ARGCOUNT_ONE,
+                DEFAULT => '',
                 EXPAND => EXPAND_VAR,
             },
         },
@@ -45,7 +46,32 @@ sub read_config {
         #
         # Ie:
         #'my_other_var' => { ARGCOUNT => 2 },
+        
         'modules' => { ARGCOUNT => ARGCOUNT_LIST },
+
+        'start_flamethrower_daemon'         => { ARGCOUNT => ARGCOUNT_ONE },
+        'flamethrower_directory_portbase'   => { ARGCOUNT => ARGCOUNT_ONE },
+        'flamethrower_directory_dir'        => { ARGCOUNT => ARGCOUNT_ONE },
+        'flamethrower_state_dir'            => { ARGCOUNT => ARGCOUNT_ONE },
+        'min_clients'                       => { ARGCOUNT => ARGCOUNT_ONE },
+        'max_wait'                          => { ARGCOUNT => ARGCOUNT_ONE },
+        'min_wait'                          => { ARGCOUNT => ARGCOUNT_ONE },
+        'async'                             => { ARGCOUNT => ARGCOUNT_ONE },
+        'autostart'                         => { ARGCOUNT => ARGCOUNT_ONE },
+        'blocksize'                         => { ARGCOUNT => ARGCOUNT_ONE },
+        'broadcast'                         => { ARGCOUNT => ARGCOUNT_ONE },
+        'fec'                               => { ARGCOUNT => ARGCOUNT_ONE },
+        'interface'                         => { ARGCOUNT => ARGCOUNT_ONE },
+        'log'                               => { ARGCOUNT => ARGCOUNT_ONE },
+        'max_bitrate'                       => { ARGCOUNT => ARGCOUNT_ONE },
+        'full_duplex'                       => { ARGCOUNT => ARGCOUNT_ONE },
+        'mcast_addr'                        => { ARGCOUNT => ARGCOUNT_ONE },
+        'mcast_all_addr'                    => { ARGCOUNT => ARGCOUNT_ONE },
+        'min_slice_size'                    => { ARGCOUNT => ARGCOUNT_ONE },
+        'slice_size'                        => { ARGCOUNT => ARGCOUNT_ONE },
+        'pointopoint'                       => { ARGCOUNT => ARGCOUNT_ONE },
+        'rexmit_hello_interval'             => { ARGCOUNT => ARGCOUNT_ONE },
+        'ttl'                               => { ARGCOUNT => ARGCOUNT_ONE },
         
     );
 
@@ -64,6 +90,15 @@ sub read_config {
                 $ft_config->modules($_);
             }
         }
+
+        #
+        # Be sure to include the flamethrower_directory in the list.
+        # This allows us to make the buid_command subroutine more
+        # generic, as $ft_config->get(${module}_dir) will work for
+        # flamethrower_directory as well as all the "normal" modules.
+        #
+        $ft_config->modules('flamethrower_directory');
+
     close(FILE);
     
     $::main::ft_config = $ft_config;
