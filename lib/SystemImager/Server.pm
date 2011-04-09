9da054353e6423d5447e15799152170c82b97db6
finley <finley@8b493298-60f9-0310-b619-f4cd4bc255a3>
2011-04-09 04:21:43 +0000

    Tweak parted bits in autoinstall script to work with newer versions of parted

--- lib/SystemImager/Server.pm
+++ lib/SystemImager/Server.pm
@@ -414,7 +414,7 @@ sub _read_partition_info_and_prepare_parted_commands {
         print $out "$cmd\n\n";
 
         print $out "# Get the size of the destination disk so that we can make the partitions fit properly.\n";
-        print $out qq(DISK_SIZE=`parted -s $devfs_dev unit MB print ) . q(| grep 'Disk geometry for' | sed 's/^.*-//g' | sed 's/\..*$//' | sed 's/MB//' `) . qq(\n);
+        print $out q(DISK_SIZE=`parted -s ) . $devfs_dev . q( unit MB print | egrep '^Disk ' | awk '{print $NF}' | sed 's/MB//' `) . qq(\n);
         print $out q([ -z $DISK_SIZE ] && shellout) . qq(\n);
 
         print $out q(if [ "$ARCH" = "alpha" ]; then) . qq(\n);	
