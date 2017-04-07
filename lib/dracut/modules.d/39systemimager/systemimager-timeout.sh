7b36a723350052e75697ae4f3bda8d995e29ebeb
olahaye74 <olivier.lahaye@cea.fr>
2017-04-07 11:05:45 +0200

    Hardened code and output. if we fail, try to find the correct iface (the 1st physical one with link) and use it in error log output (so one can check what's could lead to failure (no IP)

--- lib/dracut/modules.d/39systemimager/systemimager-timeout.sh
+++ lib/dracut/modules.d/39systemimager/systemimager-timeout.sh
@@ -14,15 +14,26 @@
 # One possible reason is that DHCP failed to get an IP address.
 
 type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
-type save_netinfo >/dev/null 2>&1 || . /lib/net-lib.sh
+#type save_netinfo >/dev/null 2>&1 || . /lib/net-lib.sh
 type shellout >/dev/null 2>&1 || . /lib/systemimager-lib.sh
+
+loginfo "==== systemimager-timeout ===="
 
 # 1st: check if we got an IP address.
 # We check that find_iface_with_link form net-lib.sh which returns 1st ethernet device with link
 # was able to get an IPv4.
 # BUG: We should test that we can ping $IMAGESERVER <OL>
 
-IFACE_WITH_LINK=`find_iface_with_link`
+IFACE_WITH_LINK=`get_1st_iface_with_link`
+if test -z "$IFACE_WITH_LINK"
+then
+    logwarn "No physical network interface found. How did we get here????"
+    shellout
+else
+    DEVICE=$IFACE_WITH_LINK
+    loginfo "Found physical network interface $DEVICE. Updating /tmp/variables.txt"
+    write_variables
+fi
 
 # 1st: try to gather all infos
 for file in variables.txt cmdline.txt dhcp_info.${IFACE_WITH_LINK}
@@ -34,7 +45,7 @@ CONFIGURED_IFACE=`ip -o -4 addr show ${IFACE_WITH_LINK}`
 echo "$CONFIGURED_IFACE" > /tmp/configured.iface
 if test -z "$CONFIGURED_IFACE"
 then
-	logwarn "Failed to get an IP address"
+	logwarn "Failed to get an IP address for iface:${IFACE_WITH_LINK}"
 	shellout
 fi
 
