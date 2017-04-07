b3c18ef99cb19d8d1fcbb430858c4c2b77646860
olahaye74 <olivier.lahaye@cea.fr>
2017-04-07 10:51:05 +0200

    Add support for old distro that lacks /etc/cmdline.d/

--- lib/dracut/modules.d/39systemimager/restore-persistent-cmdline.d.sh
+++ lib/dracut/modules.d/39systemimager/restore-persistent-cmdline.d.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-test -d /etc/persistent-cmdline.d && cp /etc/persistent-cmdline.d/* /etc/cmdline.d/
-
+if test -d /etc/cmdline.d/
+then 
+    test -d /etc/persistent-cmdline.d && cp /etc/persistent-cmdline.d/* /etc/cmdline.d/
+else
+    for FILE in /etc/persistent-cmdline.d/*
+    do
+        echo >> /etc/cmdline
+        cat $FILE >> /etc/cmdline
+    done
+fi
