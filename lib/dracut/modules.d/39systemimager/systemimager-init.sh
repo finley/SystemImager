f6eaffdaee3edfbc6986124429243bb697040c0c
olahaye74 <olivier.lahaye@cea.fr>
2017-04-19 16:59:25 +0200

    make /sbin/netroot happy so it won't try to crosscheck netroot. We only need network, no nfs root filesystem.

--- lib/dracut/modules.d/39systemimager/systemimager-init.sh
+++ lib/dracut/modules.d/39systemimager/systemimager-init.sh
@@ -7,3 +7,8 @@
 loginfo "==== systemimager-init ===="
 # Init /run/systemimager directory
 test ! -d /run/systemimager && mkdir -p /run/systemimager
+
+# make /sbin/netroot happy when called by /lib/dracut/hooks/initqueue/setup_net_<iface>.sh
+# If /sysroot/proc is present, it quits with exit status "ok" (sort of rootok)
+test ! -d /sysroot/proc && mkdir -p /sysroot/proc
+
