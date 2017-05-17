fcc45ed08a165f76e4e974b3dfc6f564968efd18
olahaye74 <olivier.lahaye@cea.fr>
2017-05-17 19:39:10 +0200

    Added a script that can be used to test and debug the systemimager plymouth theme.

---
+++ lib/dracut/modules.d/51systemimager/plymouth_theme/test_plymouth.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# SystemImager plymouth theme test suite.
+#
+
+rm -rf /tmp/plymouth.log
+. ${SYSTEMIMAGER_LIB}
+plymouthd --debug --debug-file=/tmp/plymouth.log
+plymouth show-splash
+sleep 5  # Wait for splash to appear otherwize we miss some refreshes.
+
+# Now testing.
+plymouth message --text="I:SystemImager plymouth theme test suite."
+sleep 0.5
+plymouth message --text="W: TEST: This is a warning message."
+sleep 0.5
+plymouth update --status="prei:000:000" # Highlight the preinstall script icon (0 scripts)
+sleep 0.5
+plymouth update --status="init:000:000" # Highlight the init icon
+sleep 0.5
+plymouth update --status="part:000:000" # Highlight the partition disk icon
+sleep 0.5
+plymouth update --status="frmt:000:000" # Highlight the format partition icon
+sleep 0.5
+plymouth message --text="A: Started imaging simulation."
+for i in 0{0..9}0 100 # Highlight Imaging icon and show its progress percent
+do
+    plymouth message --text="I:Progress: ${i}%"
+    plymouth update --status="imag:${i}:100"
+    sleep 0.5
+done
+plymouth update --status="boot:000:000" # Highlight the install bootloader icon
+sleep 0.5
+for i in 00{1..4} # Highlight the post install script icon and simulate 4 executions
+do
+    plymouth message --text="I:Running script ${i}/4"
+    plymouth update --status="post:$i:004"
+    sleep 0.5
+done
+plymouth message --text="N:This is the last message"
+sleep 5
+
+# Finish.
+plymouth hide-splash
+plymouth quit
+
