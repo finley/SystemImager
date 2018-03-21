261a337dbdca7f5623cfb7b9e8fbea167a1fb452
olahaye74 <olivier.lahaye@cea.fr>
2018-03-21 15:15:15 +0100

    Put rootok initialisation and /sysroot/proc creation in specific file (SuSE port requirement)

---
+++ lib/dracut/modules.d/51systemimager/systemimager-warmup.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#
+# "SystemImager"
+#
+#  Copyright (C) 1999-2017 Brian Elliott Finley <brian@thefinleys.com>
+#
+#  $Id$
+#  vi: set filetype=sh et ts=4:
+#
+#  Code written by Olivier LAHAYE.
+#
+# This file is responsible to wait for the systemimager plymouth environment.
+
+# Make sure we're not called multiple time (even if it's harmless)
+[ -e "$job" ] && rm "$job"
+
+. /lib/systemimager-lib.sh
+logdebug "==== systemimager-warmup ===="
+
+# Wait for plymouth to be ready.
+#while ! plymouth --ping
+#do
+#	sleep 1
+#done
+sleep 4
+
+# Highlight plymouth init icon.
+sis_update_step init
+
