296c4a911fd07067a7357eefe323aeff31e0e1a6
olahaye74 <olivier.lahaye@cea.fr>
2018-03-21 17:00:25 +0100

    Renamed all rd.sis. parameters to si. parameters (si = systemimager while rd is reserved for initramfs stuffs)

--- lib/dracut/modules.d/51systemimager/parse-sis-options-old.sh
+++ lib/dracut/modules.d/51systemimager/parse-sis-options-old.sh
@@ -21,8 +21,8 @@ type write_variables >/dev/null 2>&1 || . /lib/systemimager-lib.sh
 # Save cmdline SIS relevant parameters
 
 #########################
-# rd.sis.debug (defaults:N)
-test -z "$DEBUG" && DEBUG=`getarg rd.sis.debug`
+# si.debug (defaults:N)
+test -z "$DEBUG" && DEBUG=`getarg si.debug`
 [ $? -eq 0 ] && [ -z "${DEBUG}" ] && DEBUG="y" # true if parameter present with no value.
 DEBUG=`echo $DEBUG | head -c 1| tr 'YN10' 'ynyn'` # Cleann up to end with one letter y/n
 test -z "$DEBUG" && DEBUG="n" # Defaults to no.
@@ -32,117 +32,117 @@ loginfo "Reading SIS relevants parameters from cmdline"
 
 #####################################
 # imaging config file (retreived using rync) Will overwrite cmdline variables.
-# rd.sis.config="imagename.conf"
-test -z "$SIS_CONFIG" && SIS_CONFIG=`getarg rd.sis.config`
+# si.config="imagename.conf"
+test -z "$SIS_CONFIG" && SIS_CONFIG=`getarg si.config`
 
 #####################################
-# rd.sis.image-name="imagename|imagename.sh|imagename.master"
+# si.image-name="imagename|imagename.sh|imagename.master"
 test -z "$IMAGENAME" && IMAGENAME=`getarg IMAGENAME`
-test -z "$IMAGENAME" && IMAGENAME=`getarg rd.sis.image-name`
+test -z "$IMAGENAME" && IMAGENAME=`getarg si.image-name`
 
 #####################################
-# rd.sis.script-name="scriptname|scriptname.sh|scriptname.master"
+# si.script-name="scriptname|scriptname.sh|scriptname.master"
 test -z "$SCRIPTNAME" && SCRIPTNAME=`getarg SCRIPTNAME`
-test -z "$SCRIPTNAME" && SCRIPTNAME=`getarg rd.sis.script-name`
+test -z "$SCRIPTNAME" && SCRIPTNAME=`getarg si.script-name`
 
 #####################################
-# rd.sis.disks-layout="diskslayout|diskslayout.xml"
+# si.disks-layout="diskslayout|diskslayout.xml"
 test -z "$DISKS_LAYOUT" && DISKS_LAYOUT=`getarg DISKS_LAYOUT`
-test -z "$DISKS_LAYOUT" && DISKS_LAYOUT=`getarg rd.sis.disks-layout`
+test -z "$DISKS_LAYOUT" && DISKS_LAYOUT=`getarg si.disks-layout`
 
 #####################################
-# rd.sis.dl-protocol="torrent|rsync|ssh|..."
-test -z "$DL_PROTOCOL" && DL_PROTOCOL=`getarg rd.sis.dl-protocol`
+# si.dl-protocol="torrent|rsync|ssh|..."
+test -z "$DL_PROTOCOL" && DL_PROTOCOL=`getarg si.dl-protocol`
 
 #####################################
-# rd.sis.monitor-server=<hostname|ip>
+# si.monitor-server=<hostname|ip>
 test -z "$MONITOR_SERVER" && MONITOR_SERVER=`getarg MONITOR_SERVER`
-test -z "$MONITOR_SERVER" && MONITOR_SERVER=`getarg rd.sis.monitor-server`
+test -z "$MONITOR_SERVER" && MONITOR_SERVER=`getarg si.monitor-server`
 
 ###############################
-# rd.sis.monitor-port=<portnum>
-test -z "$MONITOR_PORT" && MONITOR_PORT=`getarg rd.sis.monitor-port`
+# si.monitor-port=<portnum>
+test -z "$MONITOR_PORT" && MONITOR_PORT=`getarg si.monitor-port`
 test -z "$MONITOR_PORT" && MONITOR_PORT=8181 # default value
 
 ############################################
-# rd.sis.monitor-console=(bolean 0|1|yes|no)
+# si.monitor-console=(bolean 0|1|yes|no)
 test -z "$MONITOR_CONSOLE" && MONITOR_CONSOLE=`getarg MONITOR_CONSOLE`
-test -z "$MONITOR_CONSOLE" && MONITOR_CONSOLE=`getarg rd.sis.monitor-console`
+test -z "$MONITOR_CONSOLE" && MONITOR_CONSOLE=`getarg si.monitor-console`
 [ $? -eq 0 ] && [ -z "${MONITOR_CONSOLE}" ] && MONITOR_CONSOLE="y" # true if parameter present with no value.
 MONITOR_CONSOLE=`echo $MONITOR_CONSOLE | head -c 1| tr 'YN10' 'ynyn'` # Cleann up to end with one letter y/n
 test -z "$MONITOR_CONSOLE" && MONITOR_CONSOLE="n"
 
 ###########################################
-# rd.sis.skip-local-cfg=(bolean 0|1|yes|no)
+# si.skip-local-cfg=(bolean 0|1|yes|no)
 test -z "$SKIP_LOCAL_CFG" && SKIP_LOCAL_CFG=`getarg SKIP_LOCAL_CFG`
-test -z "$SKIP_LOCAL_CFG" && SKIP_LOCAL_CFG=`getarg rd.sis.skip-local-cfg`
+test -z "$SKIP_LOCAL_CFG" && SKIP_LOCAL_CFG=`getarg si.skip-local-cfg`
 SKIP_LOCAL_CFG=`echo $SKIP_LOCAL_CFG | head -c 1| tr 'YN10' 'ynyn'` # Cleann up to end with one letter y/n
 test -z "$SKIP_LOCAL_CFG" && SKIP_LOCAL_CFG="n"
 
 ###################################
-# rd.sis.image-server=<hostname|ip>
+# si.image-server=<hostname|ip>
 test -z "$IMAGESERVER" && IMAGESERVER=`getarg IMAGESERVER`
-test -z "$IMAGESERVER" && IMAGESERVER=`getarg rd.sis.image-server`
+test -z "$IMAGESERVER" && IMAGESERVER=`getarg si.image-server`
 
 #####################################################
-# rd.sis.log-server-port=(port number) # default 8181
-test -z "$LOG_SERVER_PORT" && LOG_SERVER_PORT=`getarg rd.sis.log-server-port`
+# si.log-server-port=(port number) # default 8181
+test -z "$LOG_SERVER_PORT" && LOG_SERVER_PORT=`getarg si.log-server-port`
 test -z "$LOG_SERVER_PORT" && LOG_SERVER_PORT=514
 
 #####################################################
-# rd.sis.ssh=(bolean 0|1|yes|no|not present) => defaults to "n"
+# si.ssh=(bolean 0|1|yes|no|not present) => defaults to "n"
 test -z "$SSH" && SSH=`getarg SSH`
-test -z "$SSH" && SSH=`getarg rd.sis.ssh-client`
+test -z "$SSH" && SSH=`getarg si.ssh-client`
 [ $? -eq 0 ] && [ -z "${SSH}" ] && SSH="y" # true if parameter present with no value.
 SSH=`echo $SSH | head -c 1| tr 'YN10' 'ynyn'` # Cleann up to end with one letter y/n
 test -z "$SSH" && SSH="n" # Defaults to no.
 
 ###############################
-# rd.sis.ssh-download-url="URL"
+# si.ssh-download-url="URL"
 test -z "$SSH_DOWNLOAD_URL" && SSH_DOWNLOAD_URL=`getarg SSH_DOWNLOAD_URL`
-test -z "$SSH_DOWNLOAD_URL" && SSH_DOWNLOAD_URL=`getarg rd.sis.ssh-download-url`
+test -z "$SSH_DOWNLOAD_URL" && SSH_DOWNLOAD_URL=`getarg si.ssh-download-url`
 test -n "$SSH_DOWNLOAD_URL" && SSH="y" # SSH=y if we have a download url.
 test -n "$SSH_DOWNLOAD_URL" && test -z "${DL_PROTOCOL}" && DL_PROTOCOL="ssh"
 
 ###############################
-# rd.sis.ssh-server=(bolean 0|1|yes|no|not present) => defaults to no
+# si.ssh-server=(bolean 0|1|yes|no|not present) => defaults to no
 test -z "$SSHD" && SSHD=`getarg SSHD`
-test -z "$SSHD" && SSHD=`getarg rd.sis.ssh-server`
+test -z "$SSHD" && SSHD=`getarg si.ssh-server`
 SSHD=`echo $SSHD | head -c 1| tr 'YN10' 'ynyn'` # Cleann up to end with one letter y/n
 test -z "$SSHD" && SSHD="n" # Defaults to no.
 
 ###############################
-# rd.sis.ssh-user=<user used to initiate tunner on server>
+# si.ssh-user=<user used to initiate tunner on server>
 test -z "$SSH_USER" && SSH_USER=`getarg SSH_USER`
-test -z "$SSH_USER" && SSH_USER=`getarg rd.sis.ssh-user`
+test -z "$SSH_USER" && SSH_USER=`getarg si.ssh-user`
 [ "$SSH" = "y" ] && [ -z "$SSH_USER" ] && SSH_USER="root"
 
 ###############################################
-# rd.sis.flamethrower-directory-portbase="path"
-test -z "$FLAMETHROWER_DIRECTORY_PORTBASE" && FLAMETHROWER_DIRECTORY_PORTBASE=`getarg rd.sis.flamethrower-directory-portbase`
+# si.flamethrower-directory-portbase="path"
+test -z "$FLAMETHROWER_DIRECTORY_PORTBASE" && FLAMETHROWER_DIRECTORY_PORTBASE=`getarg si.flamethrower-directory-portbase`
 test -n "$FLAMETHROWER_DIRECTORY_PORTBASE" && test -z "${DL_PROTOCOL}" && DL_PROTOCOL="flamethrower"
 
 #########################
-# rd.sis.tmpfs-staging=""
-test -z "$TMPFS_STAGING" && TMPFS_STAGING=`getarg rd.sis.tmpfs-staging`
+# si.tmpfs-staging=""
+test -z "$TMPFS_STAGING" && TMPFS_STAGING=`getarg si.tmpfs-staging`
 
 #########################
-# rd.sis.term (Defaults to what system has defined or "linux" at last)
-# priority: rd.sis.term then system-default then "linux"
+# si.term (Defaults to what system has defined or "linux" at last)
+# priority: si.term then system-default then "linux"
 OLD_TERM=${TERM}
-TERM=`getarg rd.sis.term`
+TERM=`getarg si.term`
 test -z "${TERM}" && TERM=${OLD_TERM}
 test -z "${TERM}" -o "${TERM}" = "dumb" && TERM=linux
 
 #########################
-# rd.sis.selinux-relabel=(bolean 0|1|yes|no) => default to yes
-test -z "$SEL_RELABEL" && SEL_RELABEL=`getarg rd.sis.selinux-relabel`
+# si.selinux-relabel=(bolean 0|1|yes|no) => default to yes
+test -z "$SEL_RELABEL" && SEL_RELABEL=`getarg si.selinux-relabel`
 SEL_RELABEL=`echo $SEL_RELABEL | head -c 1 | tr 'YN10' 'ynyn'`
 test -z "$SEL_RELABEL" && SEL_RELABEL="y" # default to true!
 
 #########################
-# rd.sis.post-action=(shell, reboot, shutdown) => default to reboot
-SIS_POST_ACTION=`getarg rd.sis.post-action`
+# si.post-action=(shell, reboot, shutdown) => default to reboot
+SIS_POST_ACTION=`getarg si.post-action`
 test -z "${SIS_POST_ACTION}" && SIS_POST_ACTION="reboot"
 
 # Set a default value for protocol if it's still empty at this time.
--- lib/dracut/modules.d/51systemimager/parse-sis-options.sh
+++ lib/dracut/modules.d/51systemimager/parse-sis-options.sh
@@ -21,109 +21,109 @@ type write_variables >/dev/null 2>&1 || . /lib/systemimager-lib.sh
 # Read cmdline SIS relevant parameters
 
 #########################
-# rd.sis.debug (defaults:N)
+# si.debug (defaults:N)
 DEBUG="n"
-getargbool 0 rd.sis.debug && DEBUG="y" && logdebug "Got DEBUG=y"
+getargbool 0 si.debug && DEBUG="y" && logdebug "Got DEBUG=y"
 
 logdebug "==== parse-sis-options ===="
 loginfo "Reading SIS relevants parameters from cmdline and store them in /tmp/variables.txt"
 
 #####################################
 # imaging config file (retreived using rync) Will overwrite cmdline variables.
-# rd.sis.config="imagename.conf"
-test -z "$SIS_CONFIG" && SIS_CONFIG=$(getarg rd.sis.config) && logdebug "Got SIS_CONFIG=${SIS_CONFIG}"
+# si.config="imagename.conf"
+test -z "$SIS_CONFIG" && SIS_CONFIG=$(getarg si.config) && logdebug "Got SIS_CONFIG=${SIS_CONFIG}"
 
 #####################################
 # Name of image.
-# rd.sis.image-name="imagename|imagename.sh|imagename.master"
-test -z "$IMAGENAME" && IMAGENAME=$(getarg rd.sis.image-name -d IMAGENAME) && logdebug "Got IMAGENAME=${IMAGENAME}"
+# si.image-name="imagename|imagename.sh|imagename.master"
+test -z "$IMAGENAME" && IMAGENAME=$(getarg si.image-name -d IMAGENAME) && logdebug "Got IMAGENAME=${IMAGENAME}"
 
 #####################################
-# rd.sis.script-name="scriptname|scriptname.sh|scriptname.master"
-test -z "$SCRIPTNAME" && SCRIPTNAME=$(getarg rd.sis.script-name -d SCRIPTNAME) && logdebug "Got SCRIPTNAME=${SCRIPTNAME}"
+# si.script-name="scriptname|scriptname.sh|scriptname.master"
+test -z "$SCRIPTNAME" && SCRIPTNAME=$(getarg si.script-name -d SCRIPTNAME) && logdebug "Got SCRIPTNAME=${SCRIPTNAME}"
 
 #####################################
-# rd.sis.disk-layout="disklayout|disklayout.xml"
-test -z "$DISKS_LAYOUT" && DISKS_LAYOUT=$(getarg rd.sis.disks-layout -d DISKS_LAYOUT) && logdebug "Got DISKS_LAYOUT=${DISKS_LAYOUT}"
+# si.disk-layout="disklayout|disklayout.xml"
+test -z "$DISKS_LAYOUT" && DISKS_LAYOUT=$(getarg si.disks-layout -d DISKS_LAYOUT) && logdebug "Got DISKS_LAYOUT=${DISKS_LAYOUT}"
 
 #####################################
-# rd.sis.dl-protocol="torrent|rsync|ssh|..."
-test -z "$DL_PROTOCOL" && DL_PROTOCOL=$(getarg rd.sis.dl-protocol) && logdebug "Got DL_PROTOCOL=${DL_PROTOCOL}"
+# si.dl-protocol="torrent|rsync|ssh|..."
+test -z "$DL_PROTOCOL" && DL_PROTOCOL=$(getarg si.dl-protocol) && logdebug "Got DL_PROTOCOL=${DL_PROTOCOL}"
 
 #####################################
-# rd.sis.monitor-server=<hostname|ip>
-test -z "$MONITOR_SERVER" && MONITOR_SERVER=$(getarg rd.sis.monitor-server -d MONITOR_SERVER) && logdebug "Got MONITOR_SERVER=${MONITOR_SERVER}"
+# si.monitor-server=<hostname|ip>
+test -z "$MONITOR_SERVER" && MONITOR_SERVER=$(getarg si.monitor-server -d MONITOR_SERVER) && logdebug "Got MONITOR_SERVER=${MONITOR_SERVER}"
 
 ###############################
-# rd.sis.monitor-port=<portnum>
-test -z "$MONITOR_PORT" && MONITOR_PORT=$(getargnum 8181 100 32000 rd.sis.monitor-port) && logdebug "Got MONITOR_PORT=${MONITOR_PORT}"
+# si.monitor-port=<portnum>
+test -z "$MONITOR_PORT" && MONITOR_PORT=$(getargnum 8181 100 32000 si.monitor-port) && logdebug "Got MONITOR_PORT=${MONITOR_PORT}"
 
 ############################################
-# rd.sis.monitor-console=(bolean 0|1|yes|no)
+# si.monitor-console=(bolean 0|1|yes|no)
 MONITOR_CONSOLE="n"
 getargbool 0 MONITOR_CONSOLE && MONITOR_CONSOLE="y"
-getargbool 0 rd.sis.monitor-console && MONITOR_CONSOLE="y" && logdebug "Got MONITOR_CONSOLE=${MONITOR_CONSOLE}"
+getargbool 0 si.monitor-console && MONITOR_CONSOLE="y" && logdebug "Got MONITOR_CONSOLE=${MONITOR_CONSOLE}"
 
 ###########################################
-# rd.sis.skip-local-cfg=(bolean 0|1|yes|no)
+# si.skip-local-cfg=(bolean 0|1|yes|no)
 SKIP_LOCAL_CFG="n"
 getargbool 0 SKIP_LOCAL_CFG && SKIP_LOCAL_CFG="y"
-getargbool 0 rd.sis.skip-local-cfg && SKIP_LOCAL_CFG="y" && logdebug "Got SKIP_LOCAL_CFG=${SKIP_LOCAL_CFG}"
+getargbool 0 si.skip-local-cfg && SKIP_LOCAL_CFG="y" && logdebug "Got SKIP_LOCAL_CFG=${SKIP_LOCAL_CFG}"
 
 ###################################
-# rd.sis.image-server=<hostname|ip>
-test -z "$IMAGESERVER" && IMAGESERVER=$(getarg rd.sis.image-server -d IMAGESERVER) && logdebug "Got IMAGESERVER=${IMAGESERVER}"
+# si.image-server=<hostname|ip>
+test -z "$IMAGESERVER" && IMAGESERVER=$(getarg si.image-server -d IMAGESERVER) && logdebug "Got IMAGESERVER=${IMAGESERVER}"
 
 #####################################################
-# rd.sis.log-server-port=(port number) # default 8181
-test -z "$LOG_SERVER_PORT" && LOG_SERVER_PORT=$(getargnum 514 100 32000 rd.sis.log-server-port) && logdebug "Got LOG_SERVER_PORT=${LOG_SERVER_PORT}"
+# si.log-server-port=(port number) # default 8181
+test -z "$LOG_SERVER_PORT" && LOG_SERVER_PORT=$(getargnum 514 100 32000 si.log-server-port) && logdebug "Got LOG_SERVER_PORT=${LOG_SERVER_PORT}"
 
 ###############################
-# rd.sis.ssh-client=(bolean 0|1|yes|no|not present) Defaults to "n"
+# si.ssh-client=(bolean 0|1|yes|no|not present) Defaults to "n"
 SSH="n"
 getargbool 0 SSH && SSH="y"
-getargbool 0 rd.sis.ssh-client && SSH="y" && logdebug "Got SSH=${SSH}"
+getargbool 0 si.ssh-client && SSH="y" && logdebug "Got SSH=${SSH}"
 
 ###############################
-# rd.sis.ssh-download-url="URL"
-test -z "$SSH_DOWNLOAD_URL" && SSH_DOWNLOAD_URL=$(getarg rd.sis.ssh-download-url -d SSH_DOWNLOAD_URL) && logdebug "Got SSH_DOWNLOAD_URL=${SSH_DOWNLOAD_URL}"
+# si.ssh-download-url="URL"
+test -z "$SSH_DOWNLOAD_URL" && SSH_DOWNLOAD_URL=$(getarg si.ssh-download-url -d SSH_DOWNLOAD_URL) && logdebug "Got SSH_DOWNLOAD_URL=${SSH_DOWNLOAD_URL}"
 test -n "$SSH_DOWNLOAD_URL" && SSH="y" && loginfo "Forced SSH=y becasue SSH_DOWNLOAD_URL is set" # SSH=y if we have a download url.
 test -n "${SSH_DOWNLOAD_URL}" && test -z "${DL_PROTOCOL}" && DL_PROTOCOL="ssh" && loginfo "DL_PROTOCOL is empty. Default to 'ssh' beacause SSH_DOWNLOAD_URL is set."
 
 ###############################
-# rd.sis.ssh-server=(bolean 0|1|yes|no|not present) => defaults to no
-test -z "$SSHD" && SSHD=$(getarg rd.sis.ssh-server -d SSHD) && logdebug "Got SSHD=${SSHD}"
+# si.ssh-server=(bolean 0|1|yes|no|not present) => defaults to no
+test -z "$SSHD" && SSHD=$(getarg si.ssh-server -d SSHD) && logdebug "Got SSHD=${SSHD}"
 
 ###############################
-# rd.sis.ssh-user=<user used to initiate tunner on server>
-test -z "$SSH_USER" && SSH_USER=$(getarg rd.sis.ssh-user -d SSH_USER) && logdebug "Got SSH_USER=${SSH_USER}"
+# si.ssh-user=<user used to initiate tunner on server>
+test -z "$SSH_USER" && SSH_USER=$(getarg si.ssh-user -d SSH_USER) && logdebug "Got SSH_USER=${SSH_USER}"
 [ "$SSH" = "y" ] && [ -z "$SSH_USER" ] && SSH_USER="root" && loginfo "SSH_USER in empty. Default to 'root'"
 
 ###############################################
-# rd.sis.flamethrower-directory-portbase="path"
-test -z "$FLAMETHROWER_DIRECTORY_PORTBASE" && FLAMETHROWER_DIRECTORY_PORTBASE=$(getarg rd.sis.flamethrower-directory-portbase) && logdebug "Got FLAMETHROWER_DIRECTORY_PORTBASE=${FLAMETHROWER_DIRECTORY_PORTBASE}"
+# si.flamethrower-directory-portbase="path"
+test -z "$FLAMETHROWER_DIRECTORY_PORTBASE" && FLAMETHROWER_DIRECTORY_PORTBASE=$(getarg si.flamethrower-directory-portbase) && logdebug "Got FLAMETHROWER_DIRECTORY_PORTBASE=${FLAMETHROWER_DIRECTORY_PORTBASE}"
 test -n "$FLAMETHROWER_DIRECTORY_PORTBASE" && test -z "${DL_PROTOCOL}" && DL_PROTOCOL="flamethrower" && loginfo "DL_PROTOCOL is empty. Default to 'flamethrower' beacause FLAMETHROWER_DIRECTORY_PORTBSE is set."
 
 #########################
-# rd.sis.tmpfs-staging=""
-test -z "$TMPFS_STAGING" && TMPFS_STAGING=$(getarg rd.sis.tmpfs-staging) && logdebug "Got TMPFS_STAGING=${TMPFS_STAGING}"
+# si.tmpfs-staging=""
+test -z "$TMPFS_STAGING" && TMPFS_STAGING=$(getarg si.tmpfs-staging) && logdebug "Got TMPFS_STAGING=${TMPFS_STAGING}"
 
 #########################
-# rd.sis.term (Defaults to what system has defined or "linux" at last)
-# priority: rd.sis.term then system-default then "linux"
+# si.term (Defaults to what system has defined or "linux" at last)
+# priority: si.term then system-default then "linux"
 OLD_TERM=${TERM}
-TERM=$(getarg rd.sis.term) && logdebug "Got TERM=${TERM}"
-test -z "${TERM}" && TERM=${OLD_TERM} && loginfo "rd.sis.term is empty or not set. Using default value [$OLD_TERM]."
+TERM=$(getarg si.term) && logdebug "Got TERM=${TERM}"
+test -z "${TERM}" && TERM=${OLD_TERM} && loginfo "si.term is empty or not set. Using default value [$OLD_TERM]."
 test -z "${TERM}" -o "${TERM}" = "dumb" && TERM=linux && loginfo "No suitable TERM value. Using TERM=linux"
 
 #########################
-# rd.sis.selinux-relabel=(bolean 0|1|yes|no|y|n) => default to yes
+# si.selinux-relabel=(bolean 0|1|yes|no|y|n) => default to yes
 SEL_RELABEL="y"
-getargbool 1 rd.sis.selinux-relabel && SEL_RELABEL="y" && logdebug "Got SEL_RELABEL=y"
+getargbool 1 si.selinux-relabel && SEL_RELABEL="y" && logdebug "Got SEL_RELABEL=y"
 
 #########################
-# rd.sis.post-action=(shell, reboot, shutdown) => default to reboot
-SIS_POST_ACTION=$(getarg rd.sis.post-action) && logdebug "Got SIS_POST_ACTION=${SIS_POST_ACTION}"
+# si.post-action=(shell, reboot, shutdown) => default to reboot
+SIS_POST_ACTION=$(getarg si.post-action) && logdebug "Got SIS_POST_ACTION=${SIS_POST_ACTION}"
 test -z "${SIS_POST_ACTION}" && SIS_POST_ACTION="reboot" && loginfo "SIS_POST_ACTION is empty. Default to 'reboot'"
 
 # Set a default value for protocol if it's still empty at this time.
