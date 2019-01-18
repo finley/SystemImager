f21a31ca92dea8276ac1900e20d0c05db1d233c5
Olivier LAHAYE <olivier.lahaye1@free.fr>
2019-01-18 17:14:40 +0100

    Typo

--- lib/dracut/modules.d/51systemimager/si_inspect_client.sh
+++ lib/dracut/modules.d/51systemimager/si_inspect_client.sh
@@ -6,7 +6,7 @@ MOUNTS=`mktemp -u`
 cat /etc/fstab.systemimager|sort -b -k2 |awk '{print $1}' > $MOUNTS
 cat $MOUNTS | while read MOUNT_POINT
 do
-	echo "mountng $MOUNT_POINT"
+	echo "mounting $MOUNT_POINT"
 	mount --fstab /etc/fstab.systemimager $MOUNT_POINT
 done
 
