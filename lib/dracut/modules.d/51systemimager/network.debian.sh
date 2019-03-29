d179e193c73052c09f0f5a27f878eb2950a00221
Olivier LAHAYE <olivier.lahaye1@free.fr>
2019-03-29 14:18:52 +0100

    Skeleton for debian and suse netconfig specific

--- lib/dracut/modules.d/51systemimager/network.debian.sh
+++ lib/dracut/modules.d/51systemimager/network.debian.sh
@@ -12,8 +12,13 @@
 # This file hosts functions realted to debian specific network configuration.
 
 
-# "${IF_DEV}" "${IF_TYPE}" "${IF_NAME}" "${IF_ONBOOT}" "${IF_USERCTL}" "${IF_BOOTPROTO}" "${IF_IPADDR}" "${IF_NETMASK}" "${IF_PREFIX}" "${IF_BROADCAST}" "${IF_GATEWAY}" "${IF_IP6_INIT}" "${IF_HWADDR}" "${IF_DNS_SERVERS}" "${IF_DNS_SERVERS}"
 _write_interface() {
 	test ! -d /sysroot/etc/network/interfaces && shellout "/etc/network/interfaces not present in image."
 
+# ${IF_DEV_FULL_NAME} ${IF_HWADDR} ${IF_TYPE} ${IF_ONBOOT} ${IF_ONPARENT} ${IF_BOOTPROTO} BONDING_MASTER=$(test "${IF_TYPE}" = "Bond" && echo "yes") ${IF_DEFROUTE} ${IF_IPV4_FAILURE_FATAL} ${IF_IPADDR} ${IF_NETMASK} ${IF_PREFIX} ${IF_BROADCAST} ${IF_GATEWAY} ${IF_IPV4_ROUTE_METRIC} ${IF_PEERDNS} ${IF_MTU} ${IF_DNS1} ${IF_DNS2} ${IF_DNS3} ${IF_DOMAIN} ${IF_IPV6_INIT} ${IF_IPV6_FAILURE_FATAL} ${IF_IPV6_AUTOCONF} ${IF_IPV6_ADDR} ${IF_IPV6_DEFAULTGW} ${IF_IPV6_DEFROUTE} ${IF_IPV6_PEERDNS} ${IF_IPV6_MTU} ${IF_IPV6_ROUTE_METRIC} ${IF_FULL_NAME} "${IF_BONDING_OPTS}" ${IF_UUID} ${IF_MASTER}
+
 }
+_write_slave() {
+        test -n "${IF_BOOTPROTO/none/}" && logerror "bootproto must be none for a slave interface [${IF_NAME}]"
+}
+
--- lib/dracut/modules.d/51systemimager/network.suse.sh
+++ lib/dracut/modules.d/51systemimager/network.suse.sh
@@ -12,3 +12,13 @@
 # This file hosts functions realted to suse specific network configuration.
 
 
+_write_interface() {
+	test ! -d /sysroot/etc/sysconfig/network && shellout "/etc/sysconfig/network not present in image."
+
+# ${IF_DEV_FULL_NAME} ${IF_HWADDR} ${IF_TYPE} ${IF_ONBOOT} ${IF_ONPARENT} ${IF_BOOTPROTO} BONDING_MASTER=$(test "${IF_TYPE}" = "Bond" && echo "yes") ${IF_DEFROUTE} ${IF_IPV4_FAILURE_FATAL} ${IF_IPADDR} ${IF_NETMASK} ${IF_PREFIX} ${IF_BROADCAST} ${IF_GATEWAY} ${IF_IPV4_ROUTE_METRIC} ${IF_PEERDNS} ${IF_MTU} ${IF_DNS1} ${IF_DNS2} ${IF_DNS3} ${IF_DOMAIN} ${IF_IPV6_INIT} ${IF_IPV6_FAILURE_FATAL} ${IF_IPV6_AUTOCONF} ${IF_IPV6_ADDR} ${IF_IPV6_DEFAULTGW} ${IF_IPV6_DEFROUTE} ${IF_IPV6_PEERDNS} ${IF_IPV6_MTU} ${IF_IPV6_ROUTE_METRIC} ${IF_FULL_NAME} "${IF_BONDING_OPTS}" ${IF_UUID} ${IF_MASTER}
+
+}
+_write_slave() {
+        test -n "${IF_BOOTPROTO/none/}" && logerror "bootproto must be none for a slave interface [${IF_NAME}]"
+}
+
