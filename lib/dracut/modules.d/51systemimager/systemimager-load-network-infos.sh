b58f60c34290d425de5200e90ff1d4c614b771a5
Olivier LAHAYE <olivier.lahaye1@free.fr>
2019-07-02 09:51:03 +0200

    Removed false positive error (eth0 didn't setup correctly)

--- lib/dracut/modules.d/51systemimager/systemimager-load-network-infos.sh
+++ lib/dracut/modules.d/51systemimager/systemimager-load-network-infos.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # "SystemImager"
 #
@@ -148,7 +148,7 @@ then
 elif test -r /tmp/net.$DEVICE.override
 then
 	. /tmp/net.$DEVICE.override
-	[ ! -r /tmp/net.$DEVICE.did-setup ] && logwarn "Network $DEVICE didn't setup correctly. Trying to continue anyway."
+	[ ! -r /tmp/net.$DEVICE.did-setup -a ! -r /tmp/net.$DEVICE.up ] && logwarn "Network $DEVICE didn't setup correctly. Trying to continue anyway."
 	# We need at least IP and GW to reach the server (which is hopefully defined in cmdline)
 	[ -n "$ip" ] && IPADDR=$ip && loginfo "Got Fixed IP=$IPADDR"
 	[ -z "$IPADDR" ] && shellout "Failed to get an IP address from static config."
