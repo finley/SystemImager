66de5fb03bd6676a8a4014888741f5a687ca95d1
Olivier LAHAYE <olivier.lahaye1@free.fr>
2019-09-19 16:13:30 +0200

    Priority translated and colored to systemimager message type.

--- webgui/client_console.php
+++ webgui/client_console.php
@@ -60,27 +60,73 @@ function UpdateLogHandler(event) {
   var logText;
   try { 
     var logInfo = JSON.parse(event.data);
-    logText = "<tr><td>" + logInfo.TAG + "</td><td>" + logInfo.PRIORITY + "</td><td>" + logInfo.MESSAGE + "</td></tr>";
+    logText = "<tr><td>" + logInfo.TAG + "</td><td>" +  PriorityToText(logInfo.PRIORITY) + "</td><td>" + logInfo.MESSAGE + "</td></tr>";
   } catch (e) {
     console.error("JSON client_log parsing error: ", e);
     // console.error("JSON: ", event.data);
-    logText = "<tr><td>webgui</td><td>local0.error</td><td>" + event.data + "</td></tr>"; // BUG: invalid chars may appear and is subject to injection.
+    logText = "<tr><td>webgui</td><td>"+PriorityToText('local0.err')+"</td><td>" + event.data + "</td></tr>"; // BUG: invalid chars may appear and is subject to injection.
   }
   // console.log("log: " . logInfo.type . ": " . logInfo.message);
   // var logText = "log: " + logInfo.TAG + " - " + logInfo.PRIORITY + ": " + logInfo.MESSAGE + "<br>";
   document.getElementById("serverData").innerHTML += logText;
 }
 
+function PriorityToText(value) { // Original values from systemimager-lib.sh:logmessage()
+    switch(value) {
+        case 'local2.info': // stdout
+            return "<span class='pri_stdout'>StdOut</span>";
+            break;
+        case 'local2.err': // stderr
+            return "<span class='pri_stderr'>StdErr</span>";
+            break;
+        case 'local2.notice': // kernel info
+            return "<span class='pri_system'>Kernel</span>";
+            break;
+        case 'local1.debug': // log STEP
+            return "<span class='pri_debug'>===STEP</span>";
+            break;
+        case 'local1.info': // detail
+            return "<span class='pri_notice'>Detail</span>";
+            break;
+        case 'local1.notice': // notice
+            return "<span class='pri_notice'>Notice</span>";
+            break;
+        case 'local0.info': // info
+            return "<span class='pri_info'>Info</span>";
+            break;
+        case 'local0.warning': // warning
+            return "<span class='pri_warning'>Warning</span>";
+            break;
+        case 'local0.err': // ERROR
+            return "<span class='pri_error'>ERROR</span>";
+            break;
+        case 'local0.notice': // action
+            return "<span class='pri_action'>Action</span>";
+            break;
+        case 'local0.debug': // debug
+            return "<span class='pri_debug'>Debug</span>";
+            break;
+        case 'local0.emerg': // FATAL
+            return "<span class='pri_fatal'>FATAL</span>";
+            break;
+        default: // All other messages are system messages (not systemimager)
+            return "<span class='pri_system'>System</span>";
+            break;
+    } 
+}
+
 function StatusToText(value) {
-    if (value < 100) {
+    if (value < 0) {
+      return "Status: <span class='status0'>FAILED</span>";
+    } else if (value < 100) {
       return "Progress: " + value + "%";
     } else {
-      value -= 100;
+      var index = value - 100;
       var my_statuses = ['Imaged','Finalizing...','REBOOTED','Beeping','Rebooting...','Shutdown','Shell','Extracting...','Pre-Install','Post-Install'];
-      if ( value <= my_statuses.length+1 ) {
-        return "Status: " + my_statuses[value];
+      if ( index <= my_statuses.length+1 ) {
+        return "Status: <span class='status" + value + "'>" + my_statuses[index] + "</span>";
       } else {
-        return "Status: Unknown(" + (100 + value) + ")";
+        return "Status: Unknown (<span class='status0'>" + (value) + ")</span>";
       }
     }
 }
@@ -111,8 +157,8 @@ function UpdateClientHandler(event) {
 //                      "<br>" + get_status(clientInfo.status);
 
     var clientText2 = "CPU(s): " + clientInfo.ncpus + " x " + clientInfo.cpu +
-                      "<br>Memory: " + clientInfo.mem +
-                      "MiB<br>Kernel: " + clientInfo.kernel +
+                      "<br>Memory: " + Math.trunc(clientInfo.mem / 1024) +
+                      " MiB<br>Kernel: " + clientInfo.kernel +
                       "<br>Started: " + UnixDate(clientInfo.first_timestamp) +
                       "<br>Duration: " + (clientInfo.timestamp - clientInfo.first_timestamp) +'s';
     document.getElementById("clientData1").innerHTML = clientText1;
