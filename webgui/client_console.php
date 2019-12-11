90c59aa157e76e945162227a3c06efd364aa737a
Olivier LAHAYE <olivier.lahaye1@free.fr>
2019-12-11 09:38:22 +0100

    Add copyright and license headers to webgui files.

--- webgui/client_console.php
+++ webgui/client_console.php
@@ -1,13 +1,26 @@
 <!DOCTYPE html>
 <!--
-# vi: set filetype=php ts=4 sw=4 et :
-#
-# "SystemImager" 
-# Client console emulation. (replacement for si_monitortk console)
-#
-#  Copyright (C) 2019 Olivier LAHAYE <olivier.lahaye1@free.fr>
-#
-#
+    vi: set filetype=php ts=4 sw=4 et :
+
+    This file is part of SystemImager.
+
+    SystemImager is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    SystemImager is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SystemImager. If not, see <https://www.gnu.org/licenses/>.
+
+    Copyright (C) 2019 Olivier LAHAYE <olivier.lahaye1@free.fr>
+
+    Purpose:
+      Client console emulation. (replacement for si_monitortk console)
 -->
 
 <html>
--- webgui/edit_config.php
+++ webgui/edit_config.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
 <!--
-#
-# "SystemImager" 
-# Configure /etc/systemimager/systemimager.json
-#
-#  Copyright (C) 2019 Olivier LAHAYE <olivier.lahaye1@free.fr>
-#
-#  vi: set filetype=html et ts=4:
-#
-# Icon set: Flatwoken Icons by alecive
-#           http://www.iconarchive.com/show/flatwoken-icons-by-alecive.html
+    vi: set filetype=php ts=4 sw=4 et :
+
+    This file is part of SystemImager.
+
+    SystemImager is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    SystemImager is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SystemImager. If not, see <https://www.gnu.org/licenses/>.
+
+    Copyright (C) 2019 Olivier LAHAYE <olivier.lahaye1@free.fr>
+
+    Purpose:
+      Configure GUI for /etc/systemimager/systemimager.json
+
+    Icon set: Flatwoken Icons by alecive
+          http://www.iconarchive.com/show/flatwoken-icons-by-alecive.html
 -->
 <?php
 include 'functions.php';
