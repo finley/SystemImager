b957d3c11ac65f360b475b83e2d1693d650bdd1f
Olivier LAHAYE <olivier.lahaye1@free.fr>
2021-11-05 17:23:25 +0100

    Make sure double quotes are correctly escaped independantly of the logging system.

--- lib/dracut/modules.d/51systemimager/systemimager-lib.sh
+++ lib/dracut/modules.d/51systemimager/systemimager-lib.sh
@@ -259,7 +259,10 @@ logmessage() {
     test "${LOG_MESSAGE: -1}" == '"' && LOG_MESSAGE="${LOG_MESSAGE%\"}" # ! Keep space between : and -1
 
     # Save Message in json HTML stream.
-    HTML_ESCAPED_MSG="$(sed -E 's/\\n/<br>/g;s/\\t/\&nbsp;\&nbsp;/g;s/\&/\&amp;/g;s/>/\&gt;/g;s/</\&lt;/g;s/'\''/\&apos;/g;s/"/\\"/g;s/(ht|f)tp(s)?:\/\/[a-zA-Z0-9\.\/-]*[^.\"]/<A HREF=\\"&\\">&<\/A>/g' <<< "${LOG_MESSAGE}")"
+    # s/\\\"/\"/g;s/\"/\\\"/g will ensure that all double quotes are excaped.
+    # Message comming from journald are escaped, but messages comming from other
+    # log facility are not. this sed sequence makes thing consitent across distros.
+    HTML_ESCAPED_MSG="$(sed -E 's/\\\"/\"/g;s/\"/\\\"/g;s/\\n/<br>/g;s/\\t/\&nbsp;\&nbsp;/g;s/\&/\&amp;/g;s/>/\&gt;/g;s/</\&lt;/g;s/'\''/\&apos;/g;s/(ht|f)tp(s)?:\/\/[a-zA-Z0-9\.\/-]*[^.\"]/<A HREF=\\"&\\">&<\/A>/g' <<< "${LOG_MESSAGE}")"
     cat >> /tmp/si_report.stream <<-EOF
 LOG:{ "TAG" : "${LOG_TAG}" , "PRIORITY" : "${LOG_PRIORITY}" , "MESSAGE" : "${HTML_ESCAPED_MSG}" }
 EOF
